a9bd5462d66acfba49a34709818da1e935cc3fe7
CheFosang <fosang.che@ictuniversity.edu.cm>
2023-12-10 06:11:35 -0800

    initial commit

---
+++ src/main/java/com/exampleapp/exampleapp/HelloController.java
@@ -0,0 +1,14 @@
+package com.exampleapp.exampleapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
